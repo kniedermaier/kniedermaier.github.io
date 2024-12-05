c7689e7c46931d228839b83bbdd245a857e13bf5
kim <hello@kimniedermaier.com>
2024-12-04 19:16:33 -0800

    pwhl games!

--- src/sports/index.11tydata.js
+++ src/sports/index.11tydata.js
@@ -4,8 +4,8 @@ module.exports = {
       return data.sports.years.sort((a, b) => {
         const keyA = Number(Object.keys(a)[0]);
         const keyB = Number(Object.keys(b)[0]);
-        if (keyA < keyB) return 1;
-        if (keyA > keyB) return -1;
+        if (keyA > keyB) return 1;
+        if (keyA < keyB) return -1;
         return 0;
       });
     },
